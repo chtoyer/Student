cb2bde0d9dfa0925849561f42f31e2c2b553f378
Anna Tkacheva <annatkacheva02@mail.ru>
2026-03-26 16:08:39 +0300

    Задание #1. API Тесты. Сервис «Студент»

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,49 +1,12 @@
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
-    private String name;
-    private List<Integer> grades = new ArrayList<>();
-
-    public Student(String name) {
+    public Integer id;
+    public String name;
+    public List<Integer> grades;
+    public Student(Integer id, String name) {
+        this.id = id;
         this.name = name;
     }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public List<Integer> getGrades() {
-        return new ArrayList<>(grades);
-    }
-
-    public void addGrade(int grade) throws Exception {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet("http://localhost:5352/checkGrade?grade=" + grade);
-            try (CloseableHttpResponse httpResponse = httpClient.execute(request)) {
-                HttpEntity entity = httpResponse.getEntity();
-                String responseString = EntityUtils.toString(entity);
-                if (!Boolean.parseBoolean(responseString)) {
-                    throw new IllegalArgumentException(grade + " is wrong grade");
-                }
-                grades.add(grade);
-            }
-        }
-    }
-
-    public int raiting() throws Exception {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            int sum = grades.stream().mapToInt(x -> x).sum();
-            HttpGet request = new HttpGet("http://localhost:5352/educ?sum=" + sum);
-            try (CloseableHttpResponse httpResponse = httpClient.execute(request)) {
-                HttpEntity entity = httpResponse.getEntity();
-                return Integer.parseInt(EntityUtils.toString(entity));
-            }
-        }
-    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,37 +1,166 @@
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
 
-@WireMockTest(httpPort = 5352)
-class StudentTest {
+@WireMockTest(httpPort = 8080)
+class StudentApiTest {
 
-    @Test
-    void testAddGrade_Valid() throws Exception {
-        stubFor(get(urlPathEqualTo("/checkGrade"))
-                .withQueryParam("grade", equalTo("5"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withBody("true")));
-
-        Student student = new Student("Ivan");
-        student.addGrade(5);
-
-        assertEquals(1, student.getGrades().size());
-        assertEquals(5, student.getGrades().get(0));
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:8080";
+        com.github.tomakehurst.wiremock.client.WireMock.reset();
     }
 
     @Test
-    void testAddGrade_Invalid() throws Exception {
-        stubFor(get(urlPathEqualTo("/checkGrade"))
-                .withQueryParam("grade", equalTo("1"))
+    void testGetStudentSuccess() {
+        stubFor(get(urlEqualTo("/student/1"))
                 .willReturn(aResponse()
                         .withStatus(200)
-                        .withBody("false")));
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"id\": 1, \"name\": \"Ivan\"}")));
 
-        Student student = new Student("Ivan");
+        RestAssured.get("/student/1")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", notNullValue());
+    }
 
-        assertThrows(IllegalArgumentException.class, () -> student.addGrade(1));
-        assertTrue(student.getGrades().isEmpty());
+    @Test
+    void testGetStudentNotFound() {
+        stubFor(get(urlEqualTo("/student/999"))
+                .willReturn(aResponse().withStatus(404)));
+
+        RestAssured.get("/student/999")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void testPostStudentCreate() {
+        stubFor(post(urlEqualTo("/student"))
+                .willReturn(aResponse().withStatus(201)));
+
+        RestAssured.given().contentType(ContentType.JSON)
+                .body("{\"id\": 10, \"name\": \"Petr\"}")
+                .post("/student")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    void testPostStudentUpdate() {
+        stubFor(post(urlEqualTo("/student"))
+                .willReturn(aResponse().withStatus(201)));
+
+        RestAssured.given().contentType(ContentType.JSON)
+                .body("{\"id\": 1, \"name\": \"Ivan Updated\"}")
+                .post("/student")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    void testPostStudentNullId() {
+        stubFor(post(urlEqualTo("/student"))
+                .willReturn(aResponse()
+                        .withStatus(201)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"id\": 123, \"name\": \"NoId\"}")));
+
+        RestAssured.given().contentType(ContentType.JSON)
+                .body("{\"name\": \"NoId\"}")
+                .post("/student")
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue());
+    }
+
+    @Test
+    void testPostStudentEmptyName() {
+        stubFor(post(urlEqualTo("/student"))
+                .willReturn(aResponse().withStatus(400)));
+
+        RestAssured.given().contentType(ContentType.JSON)
+                .body("{\"id\": 5}")
+                .post("/student")
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    void testDeleteStudentSuccess() {
+        stubFor(delete(urlEqualTo("/student/1"))
+                .willReturn(aResponse().withStatus(200)));
+
+        RestAssured.delete("/student/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void testDeleteStudentNotFound() {
+        stubFor(delete(urlEqualTo("/student/888"))
+                .willReturn(aResponse().withStatus(404)));
+
+        RestAssured.delete("/student/888")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void testTopStudentEmptyDb() {
+        stubFor(get(urlEqualTo("/topStudent"))
+                .willReturn(aResponse().withStatus(200).withBody("")));
+
+        RestAssured.get("/topStudent")
+                .then()
+                .statusCode(200)
+                .body(is(emptyString()));
+    }
+
+    @Test
+    void testTopStudentNoGrades() {
+        stubFor(get(urlEqualTo("/topStudent"))
+                .willReturn(aResponse().withStatus(200).withBody("")));
+
+        RestAssured.get("/topStudent")
+                .then()
+                .statusCode(200)
+                .body(is(emptyString()));
+    }
+
+    @Test
+    void testTopStudentSingleWinner() {
+        stubFor(get(urlEqualTo("/topStudent"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[{\"id\": 2, \"name\": \"B\"}]")));
+
+        RestAssured.get("/topStudent")
+                .then()
+                .statusCode(200)
+                .body("size()", is(1))
+                .body("[0].id", is(2));
+    }
+
+    @Test
+    void testTopStudentMultipleWinners() {
+        stubFor(get(urlEqualTo("/topStudent"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[{\"id\": 1, \"name\": \"A\"}, {\"id\": 2, \"name\": \"B\"}]")));
+
+        RestAssured.get("/topStudent")
+                .then()
+                .statusCode(200)
+                .body("size()", is(2))
+                .body("name", hasItems("A", "B"));
     }
 }
